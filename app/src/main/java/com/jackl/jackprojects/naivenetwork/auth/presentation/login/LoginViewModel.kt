3e9b429595b73d33fa650acb7aa1f1b7adc256a1
jackl <iositlitt@gmail.com>
2024-12-11 19:51:53 +0900

    Toggle 액션 처리 로직 추가

--- app/src/main/java/com/jackl/jackprojects/naivenetwork/auth/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/jackl/jackprojects/naivenetwork/auth/presentation/login/LoginViewModel.kt
@@ -4,12 +4,21 @@ import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class LoginViewModel: ViewModel() {
     private val _state = MutableStateFlow(LoginState())
     val state = _state.asStateFlow()
 
     fun onAction(action: LoginAction) {
-
+        when(action) {
+            LoginAction.OnLoginClicked -> TODO()
+            LoginAction.OnToggleClicked -> {
+                _state.update {
+                    it.copy(isToggleVisible = !state.value.isToggleVisible)
+                }
+            }
+            else -> Unit
+        }
     }
 }
